d5e0b23b4bcd927a3435f79fee16bd6f2e3a8470
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-10-02 23:13:11 -0400

    Changed in db_conn file

--- iLearn/App_Start/db_conn.cs
+++ iLearn/App_Start/db_conn.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace iLearn.App_Start
 {
     public class db_conn
     {
+        SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\iLearn\\iLearn\\App_Data\\iLearn.mdf;Integrated Security=True;Connect Timeout=30");
+
+        SqlCommand cmd = new SqlCommand();
+        public void modify(string q)
+        {
+            try
+            {
+                cmd.CommandText = q;
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch 
+            { 
+                cn.Close();
+            }
+        }
     }
 }

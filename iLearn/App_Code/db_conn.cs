81ef5fe5a0ccd92f581bcad22d3fbf7436bbb04f
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-10-07 20:59:47 -0400

    Recreated db_conn file

--- iLearn/App_Code/db_conn.cs
+++ iLearn/App_Code/db_conn.cs
@@ -1,62 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.SqlClient;
 using System.Data;
+using System.Linq;
+using System.Web;
 
-namespace iLearn.App_Start
+public class db_conn
 {
-    public class db_conn
+    SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\iLearn\\iLearn\\App_Data\\iLearn.mdf;Integrated Security=True");
+
+    SqlCommand cmd = new SqlCommand();
+    public void modify(string q)
     {
-        SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\iLearn\\iLearn\\App_Data\\iLearn.mdf;Integrated Security=True");
-
-        SqlCommand cmd = new SqlCommand();
-        public void modify(string q)
+        try
         {
-            try
-            {
-                cmd.CommandText = q;
-                cmd.Connection = cn;
-                cn.Open();
-                cmd.ExecuteNonQuery();
-                cn.Close();
-            }
-            catch 
-            { 
-                cn.Close();
-            }
+            cmd.CommandText = q;
+            cmd.Connection = cn;
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
         }
-
-        public DataSet select(string q)
+        catch
         {
-            try
-            {
-                DataSet ds = new DataSet();
-                cmd.CommandText = q;
-                cmd.Connection = cn;
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                cn.Open();
-                cmd.ExecuteNonQuery();
-                da.Fill(ds);
-                cn.Close();
-                return ds;
-            }
-            catch (Exception ex)
-            {
-                cn.Close();
-                return null;
-            }
+            cn.Close();
         }
-        public int getid(string q)
+    }
+
+    public DataSet select(string q)
+    {
+        try
         {
-            cmd.CommandText= q;
-            cmd.Connection= cn;
+            DataSet ds = new DataSet();
+            cmd.CommandText = q;
+            cmd.Connection = cn;
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             cn.Open();
-            int id = (int)cmd.ExecuteNonQuery();
+            cmd.ExecuteNonQuery();
+            da.Fill(ds);
             cn.Close();
-            return id;
+            return ds;
         }
+        catch (Exception ex)
+        {
+            cn.Close();
+            return null;
+        }
+    }
+    public int getid(string q)
+    {
+        cmd.CommandText = q;
+        cmd.Connection = cn;
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        cn.Open();
+        int id = (int)cmd.ExecuteNonQuery();
+        cn.Close();
+        return id;
     }
 }

aea19f7d117802ee904099f9abd1a54d35f291f1
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-10-27 14:34:20 -0400

    Changed in ExamList page

---
+++ iLearn/ExamList.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ExamList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void grdcurrexam(object sender, GridViewCommandEventArgs e)
+    {
+
+    }
+
+  
+    protected void btnresult_Click(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void btncurrexam_Click(object sender, EventArgs e)
+    {
+
+    }
+}

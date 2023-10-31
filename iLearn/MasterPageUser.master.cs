e5391e8da3e0f915044df3edad789e8e644ee9e0
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-10-31 16:04:23 -0400

    Changed design in User master page

--- iLearn/MasterPageUser.master.cs
+++ iLearn/MasterPageUser.master.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class MasterPageUser : System.Web.UI.MasterPage
 {
+    db_conn conn = new db_conn();
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
 
+        }
+        catch(Exception)
+        {
+            Response.Redirect("Login.aspx", false);
+        }
     }
 
     protected void btnlogout_Click(object sender, EventArgs e)

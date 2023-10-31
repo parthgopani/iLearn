1e10c61843de3c4d19a70337a6f627dde340f24e
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-10-31 17:32:39 -0400

    Added registration page with code

--- iLearn/Registration.aspx.cs
+++ iLearn/Registration.aspx.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Drawing;
 
 namespace Registration
 {
@@ -20,16 +21,15 @@ namespace Registration
         //here we are extracting connection string from web.config file
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnection"].ToString());
         SqlCommand cmd = new SqlCommand();
+        SqlCommand cmd1 = new SqlCommand();
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             try
             {
-                cmd.Connection = con; //assigning connection to command
-                cmd.CommandType = CommandType.Text; //representing type of command
-                                                    //cmd.CommandText = "INSERT INTO UserDetails (Fname,Lname,Email,Password,Gender,Dob,Mobile,Address) values
-                                                    // (@Fname,@Lname,@Email,@Password,@Gender,@Dob,@Mobile,@Address)";
-                cmd.CommandText = "INSERT INTO Registration values(@Email,@F_Name,@L_Name,@Gender,@DOB,@CityId,@Add_1,@Add_2,@Password)";
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Registration values(@Email,@F_Name,@L_Name,@Gender,@DOB,@Add_1,@Add_2,@City,@Password)";
 
                 //adding parameters with value
                 cmd.Parameters.AddWithValue("@Email", txtEmail.Text.ToString());
@@ -37,15 +37,28 @@ namespace Registration
                 cmd.Parameters.AddWithValue("@L_Name", txtLastName.Text.ToString());
                 cmd.Parameters.AddWithValue("@Gender", RdoGender.SelectedItem.Text.ToString());
                 cmd.Parameters.AddWithValue("@DOB", txtDob.Text.ToString());
-                cmd.Parameters.AddWithValue("@CityId", txtCityId.Text.ToString());
                 cmd.Parameters.AddWithValue("@Add_1", txtAddress1.Text.ToString());
                 cmd.Parameters.AddWithValue("@Add_2", txtAddress2.Text.ToString());
+                cmd.Parameters.AddWithValue("@City", Textcity.Text.ToString());
                 cmd.Parameters.AddWithValue("@Password", txtPassword.Text.ToString());
+
+
+
+                cmd1.Connection = con;
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = "INSERT INTO Login values(@Email,@Password,@Type)";
+
+                //adding parameters with value
+                cmd1.Parameters.AddWithValue("@Email", txtEmail.Text.ToString());
+                cmd1.Parameters.AddWithValue("@Password", txtFirstName.Text.ToString());
+                cmd1.Parameters.AddWithValue("@Type", "u");
+
+
                 con.Open(); //opening connection
                 cmd.ExecuteNonQuery();  //executing query
+                cmd1.ExecuteNonQuery();  //executing query
                 con.Close(); //closing connection
-                lblMsg.Text = "Registered Successfully..";
-                Response.Redirect("Login.aspx");
+                lblMsg.Text = "User has been registered successfully..";
 
             }
             catch (Exception ex)
@@ -59,5 +72,6 @@ namespace Registration
             //refreshing/reloading page to clear all the controls
             Page.Response.Redirect(Page.Request.Url.ToString(), true);
         }
+
     }
 }

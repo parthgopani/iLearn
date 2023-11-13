d59c7aa6995ba1947d81721ca81acb298894224d
Parth Gopani <59224806+parthgopani@users.noreply.github.com>
2023-11-12 23:22:40 -0500

    Added complaint page

---
+++ iLearn/Complaints.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Complaints : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //    protected void btnReply_Click(object sender, EventArgs e)
+    //    {
+    //        try
+    //        {
+    //            sendEmail();
+    //            Response.Redirect("AdminDashboard.aspx");
+    //        }
+    //        catch (Exception ex)
+    //        {
+
+    //        }
+
+    //    }
+
+    //    private void sendEmail()
+    //    {
+    //        MailMessage mail = new MailMessage();
+    //        SmtpClient smtp_Client = new SmtpClient(System.Configuration.ConfigurationSettings.AppSettings["smtpClient"]);
+
+    //        smtpUserName = System.Configuration.ConfigurationSettings.AppSettings["smtpUserName"];
+    //        smtpPassword = System.Configuration.ConfigurationSettings.AppSettings["smtpPassword"];
+    //        mail.From = new MailAddress(smtpUserName);
+    //        mail.To.Add()
+    //    }
+    //
+}
